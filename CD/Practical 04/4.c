dc133aead1bd605b6b25974226712671189fac31
Devansh Parapalli <parapallidev@gmail.com>
2024-02-06 01:44:12 +0530

    parsed grammar into struct.

--- CD/Practical 04/4.c
+++ CD/Practical 04/4.c
@@ -3,9 +3,14 @@
 #include <string.h>
 #include <sys/types.h>
 
-struct Rule {
-    char lhs;
-    char * rhs[];
+#define RHS_DELIM '|'	
+#define RHS_DELIM_STR "|"
+#define LHS_DELIM ":"
+
+typedef struct Rule {
+    char * lhs;
+	size_t rhs_count;
+    char ** rhs;
 } rule_t;
 
 
@@ -55,6 +60,17 @@ size_t getline(char **buf, size_t *bufsiz, FILE *fp)
 	return getdelim(buf, bufsiz, '\n', fp);
 }
 
+void print_rule(rule_t * rule) {
+	printf("%s -> ", rule->lhs);
+	for (size_t i = 0; i < rule->rhs_count; i++) {
+		printf("%s", rule->rhs[i]);
+		if (i < rule->rhs_count - 1) {
+			printf(" | ");
+		}
+	}
+	printf("\n");
+}
+
 
 int main(int argc, char * argv[]) {
     if (argc != 2) {
@@ -63,6 +79,7 @@ int main(int argc, char * argv[]) {
     }
 
     FILE * file = fopen(argv[1], "r");
+    // FILE * file = fopen("inp2.txt", "r");
     if (file == NULL) {
         printf("Error: Unable to open file\n");
         exit(1);
@@ -72,11 +89,43 @@ int main(int argc, char * argv[]) {
     size_t len = 0;
     size_t read;
 
-    while ((read = getline(&line, &len, file)) != -1) {
-        struct Rule rule;   
+	rule_t * rules[100];
+	size_t rule_count = 0;
 
-        char *lhs = getdelim(&line, &len, ':', file);
+    while ((read = getline(&line, &len, file)) != -1) {
+        struct Rule * rule = (rule_t *)malloc(sizeof(struct Rule));   	
+		rule->rhs_count = 1;
+		for (char *ptr = line; *ptr != '\0'; ptr++) {
+			if (*ptr == RHS_DELIM) 
+				rule->rhs_count++; 
+		}
+		
+		rule->rhs = malloc(rule->rhs_count * sizeof(char *));
+		if (rule->rhs == NULL) {
+			fprintf(stderr, "Error: Unable to allocate memory @ line %d\n", __LINE__);
+			exit(EXIT_FAILURE);
+		}
+		
+		rule->lhs = strdup(strtok(line, LHS_DELIM));
+		for (size_t i = 0; i < rule->rhs_count; i++) {
+			rule->rhs[i] = strdup(strtok(NULL, RHS_DELIM_STR));
+		}
+		// remove trailing newline for last rhs using strcspn
+		rule->rhs[rule->rhs_count - 1][strcspn(rule->rhs[rule->rhs_count - 1], "\n")] = 0;
+		rules[rule_count++] = rule;
     }
 
+	fclose(file);
+	if (line) {
+		free(line);
+	}
+
+	fprintf(stdout, "Original Rules:\n");
+	for (size_t i = 0; i < rule_count; i++) {
+		print_rule(rules[i]);
+	}
+
+
+	
     return 0;
 }
